36c0cb6ef8a8de236c6c0b99934e2b77b0c13ea0
Paulo Gutiérrez <paulo.gutierrez.s.98@gmail.com>
2026-04-19 13:11:32 -0400

    Desarrolla template ProductCard.vue

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,11 +1,40 @@
 <script setup>
+import { defineProps, ref } from 'vue';
 
+const props = defineProps ({
+  product: {
+    type: Object,
+    required: true
+  }
+})
+
+const esFavorito = ref(false)
+const toggleFavorito = () => {
+  esFavorito.value = !esFavorito.value
+}
 </script>
 
 <template>
-  <div>
-    <h1>Card</h1>
-  </div>
+  <v-card flat>
+    <v-img :src="props.product.image" height="200px" cover>
+      <v-toolbar color="transparent">
+      <template #append>
+        <v-btn :icon="esFavorito ? 'mdi-heart' : 'mdi-heart-outline'" @click="toggleFavorito" :color="esFavorito ? 'red' : 'gray'"></v-btn>
+      </template>
+    </v-toolbar>
+    </v-img>
+    <v-card-item>
+      <v-card-subtitle class="font-weight-semibold">
+        ${{ props.product.price }}
+      </v-card-subtitle>
+      <v-card-title class="text-body-medium">
+        {{ props.product.title }} - {{ props.product.brand }}
+      </v-card-title>
+    </v-card-item>
+    <v-card-text class="text-medium-emphasis">
+      {{ props.product.type }} - {{ props.product.category }}
+    </v-card-text>
+  </v-card>
 </template>
 
 <style scoped>
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,13 +1,35 @@
 <script setup>
 import ProductCard from './ProductCard.vue';
 
+const products = [
+  {
+      "_id": 1,
+      "title": "Long sleeve Jacket",
+      "isNew": true,
+      "oldPrice": "200",
+      "price": 150,
+      "discountedPrice": 135,
+      "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla non magni facili blanditiis molestias soluta eveniet illum accusantium eius mollitia eligendi, ex iste doloribus magnam.",
+      "category": "women",
+      "type": "jacket",
+      "stock": 50,
+      "brand": "FashionTrend",
+      "size": [
+        "S",
+        "M",
+        "L"
+      ],
+      "image": "https://images.pexels.com/photos/2584269/pexels-photo-2584269.jpeg",
+      "rating": 4
+    },
+]
 
 </script>
 
 <template>
   <div>
     <h1>List</h1>
-    <ProductCard />
+    <ProductCard v-for="product in products" :key="product._id" :product="product"/>
   </div>
 </template>
 
