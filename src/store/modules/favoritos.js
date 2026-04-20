acca221d2eb5379e8a90c81a90d38dc0bbee4ff3
Paulo Gutiérrez <paulo.gutierrez.s.98@gmail.com>
2026-04-20 16:45:59 -0400

    Avanza módulo Favoritos

--- src/store/modules/favoritos.js
+++ src/store/modules/favoritos.js
@@ -1,4 +1,14 @@
 export default {
   namespaced: true,
-  space: {},
+  state: {
+    favoritos: [],
+  },
+  mutations: {
+    ADD_FAVOURITE(state, product) {
+      state.favourites.push(product);
+    },
+    REMOVE_FAVOURITE(state, productId) {
+      state.favourites = state.favourites.filter((product) => product._id !== productId);
+    },
+  },
 };
